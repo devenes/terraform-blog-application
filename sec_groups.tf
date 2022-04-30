af7075ae3cef4dbe3e7718f605c81108e5fbb96e
Enes <66560757+devenes@users.noreply.github.com>
2022-05-01 02:04:53 +0300

    Create sec_groups.tf

---
+++ sec_groups.tf
@@ -0,0 +1,126 @@
+##########################
+# ALB SECURITY GROUP
+##########################
+
+resource "aws_security_group" "alb-sec" {
+  name        = "alb-sec-tf"
+  description = "allow http and https"
+  vpc_id      = aws_vpc.tf_vpc.id
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+############################
+# EC2 SECURITY GROUP
+############################
+
+resource "aws_security_group" "ec2-sec" {
+  name        = "ec2-sec-tf"
+  description = "allow alb and ssh"
+  vpc_id      = aws_vpc.tf_vpc.id
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb-sec.id]
+  }
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb-sec.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+##########################
+#  RDS SECURITY GROUP
+##########################
+
+resource "aws_security_group" "rds-sec" {
+  name        = "rds-sec-tf"
+  description = "allow ec2 sec"
+  vpc_id      = aws_vpc.tf_vpc.id
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2-sec.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+##############################
+# BASTION HOST SECURITY GROUP
+##############################
+
+resource "aws_security_group" "bastion-sec" {
+  name        = "bastion-sec-tf"
+  description = "allow http https ssh"
+  vpc_id      = aws_vpc.tf_vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
